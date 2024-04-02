bbddcc0815e51ed5ba48c1b7c85099b9e20ef12b
phil-niskier cukier <felipe.cukier@gmail.com>
2024-04-02 00:24:24 +0000

    Versão 1.0

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import matplotlib as mpl
+import numpy as np
+import pandas as pd
+
+# Exibe titulo de boas vindas
+st.title('Bem vindos ao Super, Hiper, Ultra, Mega, TCHAPTCHURA!!! Sorteador de rifas do shnat 25')
+
+# Exibir tabela com participantes e quantidade
+
+# Multiplicar a lista de participantes pela quantidade de rifas que cada um comprou
+
+# Exibir a tabela com a lista multiplicada
+
+# Exibir um botato para sortear um vencedor
+
+# Ao botao clicado sortear de fato uma pessoa da lista dupçlicada 
+
+# Exibir o vencedor abaixo do botao com fonte grande e verde 
+
+# Exibir uma imagem abaixo do nome 
+
+# Tocar um som de fundo de vitoria
