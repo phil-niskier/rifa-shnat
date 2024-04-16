6ca521bdad3cca7481d2f455e273d9240ee00c78
phil-niskier cukier <felipe.cukier@gmail.com>
2024-04-16 00:24:14 +0000

    multiplicação_incompleta

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # rifa-shnat
 Códigos do sorteador de rifas do shnat - Desenvolvido em Python
+$ pip install -r requirements.txt
+$ streamlit run app.py
--- app.py
+++ app.py
@@ -13,8 +13,27 @@ def exibir_titulo(titulo):
     st.title(titulo)
 
 #Carrega os dados do csv pro dataframe
+#Limpa os dados de 'None', 'teste' ou qtd=0'
 def carregar_dados(caminho_csv):
     dados_csv=pd.read_csv(caminho_csv)
+    if 'Nome' not in dados_csv.columns or 'Quantidade de Rifas' not in dados_csv.columns:
+        st.warning("Erro: DataFrame deve conter as colunas 'Nome' e 'Quantidade de Rifas'.")
+        return
+        # Remover linhas nulas apenas nas colunas "Nome" e "Quantidade de Rifas"
+    dados_csv = dados_csv.dropna(subset=['Nome', 'Quantidade de Rifas'])
+    dados_csv = dados_csv[dados_csv['Quantidade de Rifas']>0]
+    dados_csv = dados_csv[dados_csv['Nome'] != 'teste']
+    dados_csv = dados_csv[dados_csv['Nome'] != 'Teste']
+    dados_csv['Nome'] = dados_csv['Nome'].str.title()
+    dados_csv = dados_csv.sort_values(by='Quantidade de Rifas', ascending=False)
+    for index, row in dados_csv.iterrows():
+        st.write(str(row['Quantidade de Rifas']))
+        st.write(int(row['Quantidade de Rifas']))
+    # Remover espaços em branco da coluna
+    dados_csv['Quantidade de Rifas'] = dados_csv['Quantidade de Rifas'].astype(str)
+    dados_csv['Quantidade de Rifas'] = dados_csv['Quantidade de Rifas'].str.strip()
+    # Converter a coluna para inteiro
+    dados_csv['Quantidade de Rifas'] = dados_csv['Quantidade de Rifas'].astype(int)
     return dados_csv
 
 # Exibe subtítulo
@@ -30,6 +49,21 @@ def exibir_tabela_nomes_quantidades(dados):
     #se não tiverem as colunas nome e quantidade
 
 # Multiplicar a lista de participantes pela quantidade de rifas que cada um comprou
+def multiplicar_linhas(dados_a_multiplicar):
+        # Criar uma lista para armazenar os novos registros
+    novos_registros = []
+    # Iterar sobre cada linha do DataFrame
+    for index, row in dados_a_multiplicar.iterrows():
+        nome = row['Nome']
+        quantidade = row['Quantidade de Rifas']
+        # Repetir o nome o número de vezes indicado em 'Quantidade de Rifas'
+        for _ in range(quantidade):
+            novos_registros.append({'Nome': nome})
+    
+    # Criar um novo DataFrame com os novos registros
+    novo_df = pd.DataFrame(novos_registros)
+    
+    return novo_df
 
 # Exibir a tabela com a lista multiplicada
 
@@ -39,6 +73,7 @@ def receber_arquivo():
     if arquivo is not None:
         dados=carregar_dados(arquivo)
         exibir_tabela_nomes_quantidades(dados)
+        dados_mult = multiplicar_linhas(dados)
     else:
         st.info('Voce precisa carregar uma planilha')
 
